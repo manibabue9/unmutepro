2fb698a3054a248581a9126c7ba10b26f4655de3
manibabue9 <mani.babu52@gmail.com>
2026-08-01 03:06:01 +0530

    Add resources page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://www.unmutepro.com/resources",
+      lastModified: new Date("2026-08-01"),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://www.unmutepro.com/blog",
       lastModified: new Date("2026-08-01"),
       changeFrequency: "weekly",
